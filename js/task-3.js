32f672a56c718c54a6b0eed7f45d6a1d0d647e9f
YaninaBrizg <yanina.brizg@gmail.com>
2023-12-14 18:02:36 +0100

    homewook2

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,16 @@
-function getElementWidth(content, padding, border) {
-  let borderBox =
-    Number.parseFloat(content) +
-    2 * (Number.parseFloat(padding) + Number.parseFloat(border));
-  return borderBox;
+const banWord1 = `spam`;
+const banWord2 = `sale`;
+function checkForSpam(message) {
+  return (
+    message.toLowerCase().includes(banWord1) ||
+    message.toLowerCase().includes(banWord2)
+  );
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
